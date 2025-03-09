47827e2a9e81a529d69c20861176f3b255cc5792
mehardiknaik <96820742+mehardiknaik@users.noreply.github.com>
2025-03-09 12:00:44 +0530

    Update page.tsx

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,16 +1,24 @@
-"use client"
-import ScrollingText from "web-scrolling-text/react"
+"use client";
+import Image from "next/image";
+import ScrollingText from "web-scrolling-text/react";
 
 const basePath = process.env.PAGES_BASE_PATH || "";
 
 export default function Home() {
   return (
-   <div>
-    <ScrollingText>
-      <h1>Home Page</h1>
-      <p>This is the home page</p>
-      <p>{basePath}</p>
-    </ScrollingText>
-   </div>
+    <div>
+      <Image
+        className="dark:invert"
+        src={`${basePath}/next.svg`}
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <ScrollingText>
+        <h1>Home Page</h1>
+        <p>This is the home page</p>
+      </ScrollingText>
+    </div>
   );
 }
