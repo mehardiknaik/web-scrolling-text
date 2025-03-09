03ccad7bc0a36f53092cbc457d67631122cb39a8
mehardiknaik <96820742+mehardiknaik@users.noreply.github.com>
2025-03-09 10:56:26 +0530

    Update next.config.ts

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: isProd ? '/web-scrolling-text' : '',
+  basePath: isProd ? '/web-scrolling-text' : '',
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
 };
 
 export default nextConfig;
